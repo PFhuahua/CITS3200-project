c6d1f0c23bd49d3c48475a2215a0dc0748e0889c
James-Felstead <23962355@student.uwa.edu.au>
2025-08-21 21:23:36 +0800

    testCensusWS.py Only produces Unqiue URLs

--- GoogleSearch_WS/testCensusWS.py
+++ GoogleSearch_WS/testCensusWS.py
@@ -4,18 +4,18 @@ import time
 start = time.time() 
 
 
-Country = "USA"
-Year = "1950"
+Country = ""
+Year = ""
 
 filters = ["wikipedia"]
 
-othertags = ["census","migration","data"]
+othertags = ["SEGUNDO CENSO DE LA REPUBLICA ARGENTINA MAYO 10 DE 1895 368 PRIMEROS RESULTADOS MAYO 10 DE 1896 COMISIÓN DIRECTIVA: DIEGO G.. DE LA FUENTE, Presidente. GABRIEL. CARRASCO- ALBERTO B. MARTiNEZ Vocales,"]
 othertags = ' '.join(f'"{tag}"' for tag in othertags)
 
-Query = "\""+Country +"\" " +"\""+Year+"\" " + othertags
+Query = othertags
 
 results = []
-for link in search(Query , num_results=10):
+for link in search(Query , num_results=5):
         if not any(f in link for f in filters):
               
               if(".pdf" in link): results.append(link)
@@ -28,8 +28,11 @@ for link in search(Query , num_results=10):
                     if len(results) > 10: break
         if len(results) > 5: break
 
+unique_results = list(set(results))
+
+
 print(f"\n\nPDF RESULTS FOR {Query}:\n")
-for i, url in enumerate(results, start=1):
+for i, url in enumerate(unique_results, start=1):
     print(f"{i}. {url}")
 print("\n")
 end = time.time()
