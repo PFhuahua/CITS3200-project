c1c9901a5c56874a4e798eea0dd02a0676a7bdf9
James-Felstead <23962355@student.uwa.edu.au>
2025-08-21 20:46:20 +0800

    testCensusWS.py Made the tag system more general, added time logger

--- GoogleSearch_WS/testCensusWS.py
+++ GoogleSearch_WS/testCensusWS.py
@@ -1,15 +1,21 @@
 from googlesearch import search
 from ScraperTool import scrape_pdfs
+import time
+start = time.time() 
 
 
-Country = "\"Australia\""
-Year = "\"2020\""
+Country = "USA"
+Year = "1950"
 
 filters = ["wikipedia"]
 
-Query = Country + Year + "\"census\"\"migration\"\"data\""
+othertags = ["census","migration","data"]
+othertags = ' '.join(f'"{tag}"' for tag in othertags)
+
+Query = "\""+Country +"\" " +"\""+Year+"\" " + othertags
+
 results = []
-for link in search(Query , num_results=24):
+for link in search(Query , num_results=10):
         if not any(f in link for f in filters):
               
               if(".pdf" in link): results.append(link)
@@ -17,12 +23,14 @@ for link in search(Query , num_results=24):
               else: 
                 scraped = scrape_pdfs(link)
                 if scraped == None: continue
-
                 for item in scraped:
                     results.append(item["url"])
-        if len(results) > 4: break
+                    if len(results) > 10: break
+        if len(results) > 5: break
 
 print(f"\n\nPDF RESULTS FOR {Query}:\n")
 for i, url in enumerate(results, start=1):
     print(f"{i}. {url}")
 print("\n")
+end = time.time()
+print(f"Elapsed time: {end - start:.4f} seconds")
