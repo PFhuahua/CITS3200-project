ab5746b1f1a160730059de314bc59fb2b484536d
James-Felstead <23962355@student.uwa.edu.au>
2025-08-25 12:59:36 +0800

    Changed search tags in testCensusWS.py to be general

--- GoogleSearch_WS/testCensusWS.py
+++ GoogleSearch_WS/testCensusWS.py
@@ -2,21 +2,23 @@ from googlesearch import search
 from ScraperTool import scrape_pdfs
 from ScraperTool import process_pdf_link
 import time
-start = time.time() 
 
-
-Country = ""
-Year = ""
 
 filters = ["wikipedia"]
-
-othertags = ["BEVÖLKERUNG UND KULTUR Volkszählung vom 27. Mai 1970 Heft 15 Bevölkerung nach überwiegendem Lebensunterhalt und Beteiligung am Erwerbsleben"]
-Exact_tags = False
+ExactTags = []
+NonExactTags = ["Kenya","census","migration","data","2020"]
 Filesize = None
 
-if Exact_tags: othertags = ' '.join(f'"{tag}"' for tag in othertags)
 
-Query = othertags
+start = time.time() 
+
+if ExactTags != []: ExactTags = ' '.join(f'"{tag}"' for tag in ExactTags)
+else: ExactTags = " "
+
+if NonExactTags != []: NonExactTags = ' '.join(NonExactTags)
+else: NonExactTags = " "
+
+Query = ExactTags + " " + NonExactTags
 
 FULL_RESULTS = []
 results = []
@@ -28,16 +30,16 @@ for link in search(Query, num_results=20):
             FULL_RESULTS.append(pdf_info)
             results.append((pdf_info["url"], pdf_info["size"]))  # store as (url, size) pair
         else:
-            # Non-PDF link → scrape for PDFs
+            # Non-PDF link -> scrape for PDFs
             scraped = scrape_pdfs(link)
             if scraped is None:
                 continue
             for item in scraped:
                 FULL_RESULTS.append(item)
                 results.append((item["url"], item["size"]))  # store as (url, size) pair
-                if len(results) > 10:  # break inner loop if too many
+                if len(results) > 4:  # break inner loop if too many
                     break
-    if len(results) > 10:  # break outer loop if too many
+    if len(results) > 4:  # break outer loop if too many
         break
 
 unique_results = list(set(results))
