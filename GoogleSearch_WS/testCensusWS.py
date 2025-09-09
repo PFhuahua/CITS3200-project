e4e37cbe36ae9429640055bb67dd28c8fba8a1a3
James-Felstead <23962355@student.uwa.edu.au>
2025-09-09 18:41:34 +0800

    Swapped testCensusWS.py to use Google Custom API

--- GoogleSearch_WS/testCensusWS.py
+++ GoogleSearch_WS/testCensusWS.py
@@ -1,19 +1,23 @@
+import requests
 from googlesearch import search
 from ScraperTool import scrape_pdfs
 from ScraperTool import process_pdf_link
 import time
 
-Filters = ["wikipedia","statista","worldbank","unstats"]
+
+
+Filters = ["wikipedia","statista","worldbank","unstats","usa.ipums.org","international.ipums.org","redatam.org","ourworldindata","www.un.org","www.oecd."]
 ExactTags = []
-NonExactTags = ["Uganda","census","migration","data","2014"]
+NonExactTags = ["Census", "Uganda","2018","migration"]
 Filesize = None
 MaxPDFs = 5
-ResultsSearched = 10
+ResultsSearched = 10 # 10 IS MAX IN THIS VERSION
+
+api_key = "PUT API KEY HERE" # API KEY
+Searchcx = "f492a293c453341a1"
 
 
-
-
-start = time.time() 
+start = time.time()
 
 if ExactTags != []: ExactTags = ' '.join(f'"{tag}"' for tag in ExactTags)
 else: ExactTags = " "
@@ -22,14 +26,36 @@ if NonExactTags != []: NonExactTags = ' '.join(NonExactTags)
 else: NonExactTags = " "
 
 Query = ExactTags + " " + NonExactTags
+url = f"https://www.googleapis.com/customsearch/v1?q={Query}&key={api_key}&cx={Searchcx}&num={ResultsSearched}"
 
 FULL_RESULTS = []
 results = []
-for link in search(Query, num_results= ResultsSearched):
+
+Links = []
+
+response = requests.get(url)
+print(response.status_code)
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+    # Loop over the search results
+    for item in data.get('items', []):
+        link = item['link']          # URL
+        title = item['title']        # Title of page
+        snippet = item['snippet']    # Description of page
+        Links.append(link)
+else:
+    print("Error:", response.status_code, response.text)
+
+
+
+for link in Links:
     if not any(f in link for f in Filters):
         if ".pdf" in link.lower():
             # Process PDF link
             pdf_info = process_pdf_link(link)
+            if pdf_info == None: 
+                continue
             FULL_RESULTS.append(pdf_info)
             results.append((pdf_info["url"], pdf_info["size"]))  # store as (url, size) pair
         else:
@@ -53,5 +79,6 @@ for i, url in enumerate(unique_results, start=1):
     if url[1] == None: print(f"{i}. {url[0]} file size: UNKNOWN")
     else: print(f"{i}. {url[0]}, file size: {round(url[1]/1000,0)} KB")
 print("\n")
+
 end = time.time()
 print(f"Elapsed time: {end - start:.4f} seconds")
