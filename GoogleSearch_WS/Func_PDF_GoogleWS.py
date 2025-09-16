3528afb550420aa5e004c5a25b9592db2b66375f
James-Felstead <23962355@student.uwa.edu.au>
2025-09-16 16:51:39 +0800

    Added google LIB search to Func_PDF_GoogleWS.py

--- GoogleSearch_WS/Func_PDF_GoogleWS.py
+++ GoogleSearch_WS/Func_PDF_GoogleWS.py
@@ -88,3 +88,74 @@ def PDF_Google_WS(
     print("\n")
 
     return(unique_results) # PDF URL, PDF size (bytes), PDF Title, PDF Snippet
+
+def LIB_Google_WS(
+    NonExactTags: str = None,
+    ExactTags: str = None,
+    Filters: list[str] = [".docx"],
+    MaxHTML: int = 5,
+    ResultsSearched: int = 5,
+    api_key: str = "API",
+    Searchcx: str = "97c19d00f487341b6"):
+
+    """
+    Search Lib matches using Google Custom Search API and Return Results into a List of Tuples.
+    
+    Parameters:
+    - ExactTags: list of strings to match exactly
+    - NonExactTags: list of strings to include in search
+    - Filters: list of strings to exclude from results (domains or file types)
+    - MaxPDFs: max number of PDFs to return
+    - ResultsSearched: total number of search results to scan
+    - api_key: Google API key
+    - Searchcx: Google Custom Search Engine ID
+    
+    Returns:
+    - unique_results: list of tuples (PDF URL, PDF size in bytes or None, Title, Snippet)
+    """
+
+    if ExactTags == None: ExactTags = " "
+    if NonExactTags == None : NonExactTags = " "
+
+    Query = ExactTags + " " + NonExactTags
+
+    results = []
+    start_search_index = 0 # Start index of searches (1)
+
+    while start_search_index <= ResultsSearched:
+        # Determine how many results to request in the search (API only allows a max of 10 per search)
+        num_results = min(ResultsSearched - start_search_index + 1, 10)
+        
+        url = f"https://www.googleapis.com/customsearch/v1?q={Query}&key={api_key}&cx={Searchcx}&num={num_results}&start={start_search_index}"
+        response = requests.get(url)
+        print(f"Requesting results {start_search_index}-{start_search_index+num_results-1}, status:", response.status_code)
+
+        if response.status_code == 200:
+            data = response.json()
+            if int(data['searchInformation']['totalResults']) == 0: break
+            for item in data.get('items', []):
+                link = item['link']
+                print(f"\n{link}\n")
+                title = item['title']
+                snippet = item['snippet']
+
+                if not any(f in link for f in Filters):   
+                    print(link)
+                    results.append([link,title,snippet])
+                if len(results) >= MaxHTML:
+                    break
+        else:
+            print("Error:", response.status_code, response.text)
+        
+        if len(results) >= MaxHTML:
+            break
+        
+        start_search_index += num_results  # Move to next block of results
+
+
+    print(f"\n\nLibrary RESULTS FOR {Query}:\n")
+    for i, url in enumerate(results, start=1):
+        print(f"{i}. {url[0]} \nTitle: {url[1]}, \nsnippet: {url[2]}\n")
+    print("\n")
+
+    return(results) # Website URL, Website Title, Website Snippet    
