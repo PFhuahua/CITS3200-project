bd6061f1ffaf98351eacf33e16bc10c74a4bf41b
James-Felstead <23962355@student.uwa.edu.au>
2025-09-22 11:30:50 +0800

    Updated LibraryTest.py with more national libraries

--- GoogleSearch_WS/Unit_Tests/LibraryTest.py
+++ GoogleSearch_WS/Unit_Tests/LibraryTest.py
@@ -5,7 +5,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import re
 from SpecifiedLibraryFunc import scrape_library  # type: ignore
 
-libsToCheck = ["France"] 
+libsToCheck = [" "] 
 Search = "Census"
 ResultsPerLib = 1
 
@@ -105,11 +105,76 @@ Library = {
         "Result_URL_Start": "https://webggc.oclc.org/cbs/DB=2.37/SET=4/TTL=1/",
         "Visible": True,
         "CAPTCHA": False
+    },
+    "Portugal":{
+        "Name": "National Library of Portugal",
+        "URL_Start": "https://bndigital.bnportugal.gov.pt/records?navigation=default&perpage=10&page=1&search=",
+        "URL_End": "&fulltext=1&child=1&bookmarks=1&sort=_score#page",
+        "SearchSelector": "div.navlist_img",
+        "Attribute": {},
+        "tag": "div",
+        "tag_class": "navlist_content",
+        "ResultSelector": 'img.scale-with-grid',
+        "Result_URL_Start": "https://bndigital.bnportugal.gov.pt",
+        "Visible": True,
+        "CAPTCHA": False
+    },
+    "Canada":{
+        "Name": "Library and Archives Canada",
+        "URL_Start": "https://www.canada.ca/en/sr/srb.html#q=",
+        "URL_End": "",
+        "SearchSelector": "div.results",
+        "Attribute": {"class":"result-link"},
+        "tag": "div",
+        "tag_class": "results",
+        "ResultSelector": 'div.mwsgeneric-base-html',
+        "Result_URL_Start": "",
+        "Visible": True,
+        "CAPTCHA": False
+    },
+    "London":{
+        "Name": "London School of Economics and Political Science",
+        "URL_Start": "https://librarysearch.lse.ac.uk/discovery/search?query=any,contains,",
+        "URL_End": "&tab=Everything&search_scope=MyInst_and_CI&vid=44LSE_INST:44LSE_VU1&offset=0",
+        "SearchSelector": "img.main-img",
+        "Attribute": {"ng-class": "::{'full-view-mouse-pointer':$ctrl.isFullView}"},
+        "tag": "h3",
+        "tag_class": "item-title",
+        "ResultSelector": "img.main-img",
+        "Result_URL_Start": "",
+        "Visible": True,
+        "CAPTCHA": False
+    },
+    "United States":{
+        "Name": "United States Census Bureau",
+        "URL_Start": "https://www.census.gov/search-results.html?q=",
+        "URL_End": "&page=1&stateGeo=none&searchtype=web&cssp=SERP&_charset_=UTF-8",
+        "SearchSelector": "div.uscb-search-result__page-snippet",
+        "Attribute": {"onclick":"searchCustomEvent('ORG:https:\/\/www.census.gov\/about\/history\/bureau\\u002Dhistory\/census\\u002Dpeople\/census\\u002Ddirectors.html'"},
+        "tag": "div",
+        "tag_class": "search-results",
+        "ResultSelector": "img.main-img",
+        "Result_URL_Start": "",
+        "Visible": True,
+        "CAPTCHA": False
+    },
+    "National Archives":{
+        "Name": "United States National Archives",
+        "URL_Start": "https://search.archives.gov/search?affiliate=national-archives&query=",
+        "URL_End": "",
+        "SearchSelector": "img.usa-identifier__logo",
+        "Attribute": {"class": "result-title-link"},
+        "tag": "div",
+        "tag_class": "search-result-item-wrapper",
+        "ResultSelector": "div.col-sm-9",
+        "Result_URL_Start": "",
+        "Visible": True,
+        "CAPTCHA": False
     }
 }
 
 
-#for libs in Library: print(Library[libs]["Name"])
+for libs in Library: print(Library[libs]["Name"])
 #for libs in Library: print(Library[libs]["Attribute"])
 
 start = time.time()
